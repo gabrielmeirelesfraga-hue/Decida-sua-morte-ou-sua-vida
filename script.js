594e852cc4d5c840fb35f66604da2b1aa7c0a36b
gabrielmeirelesfraga-hue <gabriel.meireles.fraga@escola.pr.gov.br>
2025-11-06 16:19:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-próximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo')
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-próximo')
 
         atual.classList.remove('ativo')
         document.getElementById(proximoPasso).classList.add('ativo')
